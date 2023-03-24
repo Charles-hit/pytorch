1861d5047e58c7a5eaeac503234df1f3e3a16386
Michael Voznesensky <voznesenskym@gmail.com>
2023-03-24 02:56:50 +0000

    Adjusted design for ShapeEnv.
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    ghstack-source-id: 40f01739ce5ef971597444b7d2d450ac15a381cc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/97164
    
    Pass some tests
    
    fixing
    
    fixing
    
    Fix misalloc
    
    Fix more
    
    Fixes
    
    Fix
    
    none fix

--- test/functorch/test_aotdispatch.py
+++ test/functorch/test_aotdispatch.py
@@ -395,7 +395,7 @@ class TestAOTAutograd(AOTTestCase):
 
         self.verify_aot_autograd(F(), inp)
 
-    def test_embedding_bag_view(self):
+    def test_embedding_bag_view_dynamic(self):
         # Backwards pass tries to wrap a sparse tensor in a FunctionalTensorWrapper;
         # test that this works even though the sparse tensor has no storage.
 
@@ -409,7 +409,10 @@ class TestAOTAutograd(AOTTestCase):
 
         x = torch.arange(3)
         y = torch.arange(3)
+        self.verify_aot_autograd(F(), [x, y], dynamic=False)
         self.verify_aot_autograd(F(), [x, y], dynamic=True)
+
+
 
     @patch("functorch.compile.config.use_fake_tensor", True)
     def test_input_mutation_simple(self):
